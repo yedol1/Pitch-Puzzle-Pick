0308668877273b3f7244f09d775c0a45d93c0e88
10_2pang <57481378+yedol1@users.noreply.github.com>
2023-09-19 01:19:36 +0900

    fix: 초기 스쿼드 세팅 제거

--- app/player/[UID]/page.tsx
+++ app/player/[UID]/page.tsx
@@ -27,70 +27,68 @@ const PlayerDetail = () => {
     );
 
   return (
-    playerData && (
-      <section className='inline-flex justify-center items-start content-start gap-[87px] flex-wrap'>
-        <h1 className='sr-only'>선수 디테일 페이지</h1>
-        <div className='w-[464px] flex flex-col'>
-          <div className='w-full flex flex-row items-start mb-[16px]'>
-            <button onClick={() => router.back()}>
-              <Image src='/back.svg' width={138} height={50} alt='뒤로가기 버튼' />
-            </button>
-          </div>
-          <PlayerDetailInfo playerData={playerData} />
-          <div className='w-[464px] h-[2px] flex-shrink-0 bg-[#F75050] mt-[10px] mb-[30px]'></div>
-          {playerData.DetailedPos !== 'GK' ? (
-            <Tabs value='DEFAULT'>
-              <TabsHeader>
-                {data.map((value) => (
-                  <Tab key={value} value={value}>
-                    {value}
-                  </Tab>
-                ))}
-              </TabsHeader>
-              <TabsBody>
-                {data.map((value) => (
-                  <TabPanel key={value} value={value}>
-                    {value === 'DEFAULT' ? (
-                      <FieldPlayerMainStat playerData={playerData} />
-                    ) : (
-                      <FieldPlayerGraphStat playerData={playerData} />
-                    )}
-                  </TabPanel>
-                ))}
-              </TabsBody>
-            </Tabs>
-          ) : (
-            <Tabs value='DEFAULT'>
-              <TabsHeader>
-                {data.map((value) => (
-                  <Tab key={value} value={value}>
-                    {value}
-                  </Tab>
-                ))}
-              </TabsHeader>
-              <TabsBody>
-                {data.map((value) => (
-                  <TabPanel key={value} value={value}>
-                    {value === 'DEFAULT' ? (
-                      <GkMainStat playerData={playerData} />
-                    ) : (
-                      <GkMainGraphStat playerData={playerData} />
-                    )}
-                  </TabPanel>
-                ))}
-              </TabsBody>
-            </Tabs>
-          )}
+    <section className='inline-flex justify-center items-start content-start gap-[87px] flex-wrap'>
+      <h1 className='sr-only'>선수 디테일 페이지</h1>
+      <div className='w-[464px] flex flex-col'>
+        <div className='w-full flex flex-row items-start mb-[16px]'>
+          <button onClick={() => router.back()}>
+            <Image src='/back.svg' width={138} height={50} alt='뒤로가기 버튼' />
+          </button>
         </div>
-        <div className='flex p-[22px] py-[37px] items-start gap-[10px] rounded-[16.386px] bg-white shadow-custom'>
-          {playerData.DetailedPos !== 'GK' ? (
-            <FieldPlayerDetailStat playerData={playerData} />
-          ) : (
-            <GkDetailStat playerData={playerData} />
-          )}
-        </div>
-      </section>
-    )
+        <PlayerDetailInfo playerData={playerData} />
+        <div className='w-[464px] h-[2px] flex-shrink-0 bg-[#F75050] mt-[10px] mb-[30px]'></div>
+        {playerData.DetailedPos !== 'GK' ? (
+          <Tabs value='DEFAULT'>
+            <TabsHeader>
+              {data.map((value) => (
+                <Tab key={value} value={value}>
+                  {value}
+                </Tab>
+              ))}
+            </TabsHeader>
+            <TabsBody>
+              {data.map((value) => (
+                <TabPanel key={value} value={value}>
+                  {value === 'DEFAULT' ? (
+                    <FieldPlayerMainStat playerData={playerData} />
+                  ) : (
+                    <FieldPlayerGraphStat playerData={playerData} />
+                  )}
+                </TabPanel>
+              ))}
+            </TabsBody>
+          </Tabs>
+        ) : (
+          <Tabs value='DEFAULT'>
+            <TabsHeader>
+              {data.map((value) => (
+                <Tab key={value} value={value}>
+                  {value}
+                </Tab>
+              ))}
+            </TabsHeader>
+            <TabsBody>
+              {data.map((value) => (
+                <TabPanel key={value} value={value}>
+                  {value === 'DEFAULT' ? (
+                    <GkMainStat playerData={playerData} />
+                  ) : (
+                    <GkMainGraphStat playerData={playerData} />
+                  )}
+                </TabPanel>
+              ))}
+            </TabsBody>
+          </Tabs>
+        )}
+      </div>
+      <div className='flex p-[22px] py-[37px] items-start gap-[10px] rounded-[16.386px] bg-white shadow-custom'>
+        {playerData.DetailedPos !== 'GK' ? (
+          <FieldPlayerDetailStat playerData={playerData} />
+        ) : (
+          <GkDetailStat playerData={playerData} />
+        )}
+      </div>
+    </section>
   );
 };
 
